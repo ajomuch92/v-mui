c08eb6ba3eca5227e07aef6f2e47835323571e3c
ajomuch92 <aaronmontes_92@yahoo.es>
2022-07-10 18:25:02 -0600

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VMuiSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  VMuiSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <v-mui-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VMuiSample } from './v-mui-sample.vue';
